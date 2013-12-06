f36767548b52413d07a8b900094f03b4ea7a07da
Debbie Reches <dreches@fas.harvard.edu>
2013-12-05 22:59:31 -0500

    synching up the quote and clue inputs

--- js/acrostic.js
+++ js/acrostic.js
@@ -42,12 +42,20 @@ function getSelectionEnd(o) {
 };
 */
 
+
+
 function lastCursorPosition(pos){
 	
 	if (!pos) return (this.lastpos || -2);
 	else this.lastpos = pos;
 	return this.lastpos;
 };	
+
+(function($) {
+	$.fn.exists = function () {
+		return this.length !== 0;
+	}
+})(jQuery);
 
 (function($) {
 	$.fn.getCursorPosition = function() {
@@ -62,15 +70,26 @@ function lastCursorPosition(pos){
 	 }
 })(jQuery);
 
-function getInputID (element)
-{
-	return Number(element.attr("id").substr(1));
+function idVal (idAttribute) {
+	if (!idAttribute) return 0;
+	if ( idAttribute.indexOf("td")> -1)
+		return Number(idAttribute.substr(3));
+	else 
+		return Number(idAttribute.substr(2));
+}
+
+function idTag( idAttribute) {
+	if (!idAttribute) return NULL;
+	if ( idAttribute.indexOf("td")> -1)
+		return idAttribute.substr(0,3);
+	else return idAttribute.substr(0,2);
 }
 
 
 
 $(document).ready(function()
 {
+	var startValue;
 	$("#clues-div a.clue_id").on("click", function() {
 		var selected = $( "table input.display_bright");
 		selected.toggleClass("display_bright",false);
@@ -78,46 +97,76 @@ $(document).ready(function()
 		var clueID = $(this).attr("id").substr(5);
 		$("table#quote input."+clueID).toggleClass("display_bright",true);
 	});
-	$( "table" ).on( "focusin", "input", function() {
-		//alert( $( this ).text() );
-		//$(this).css("background-color","red");
-		$(this).toggleClass("highlight");
-		console.log("FOCUSIN: value=["+$(this)[0].value+"]");
-	});
-
-	$("table#quote").on({ 	click: function() {
+	$( "table" ).on( {	click: function() {
 								this.select();
 							},
-							focusin: function() {
-								var content = $(this)[0].value;
-								lastCursorPosition($(this).getCursorPosition());
-								/*
-								if (content.length > 0)
-								{	
-									//$(this).get(0).focus();
-									$(this).select();
-								};*/
-							},
+						focusin: function() {		
+							var startValue = $(this).val();
+							console.log("FOCUSIN: value=["+content+"]");				
+							lastCursorPosition($(this).getCursorPosition());
+							var id = $(this).attr("id");
+							var selector = "[id$=i"+idVal(id)+"]";
+							var $currentInputs = $("input"+selector);
+							//$(this).toggleClass("highlight");
+							$currentInputs.toggleClass("highlight",true);
+							/*
+							if (content.length > 0)
+							{	
+								//$(this).get(0).focus();
+								$(this).select();
+							};*/
+						},
+						
+						focusout: function() {	
+							var id = $(this).attr("id");
+							var selector = "[id$=i"+idVal(id)+"]";
+							var $currentInputs = $("input"+selector);
+							$currentInputs.toggleClass("highlight",false);
+							console.log("focusout event");
+							$(this).css("color","black");
+						},
+						keyup: function() {
+							var keyValue = $(this).val().toUpperCase();
+							var id = $(this).attr("id");
+							var selector = "[id$=i"+idVal(id)+"]";
+							var $currentInputs = $("input"+selector);
+							var highlighted = $( "table input.highlight_text");
+							$( "table input.highlight_text").toggleClass("highlight_text",false);
+							if (startValue != keyValue) {
+								$currentInputs.toggleClass("highlight_text",true);
+							} 
 							
-							focusout: function() {				
-								$(this).toggleClass("highlight");
-								//console.log("focusout event");
-								$(this).css("color","black");
-							},
-							change: function() {
+							$currentInputs.each( function() {
+													$(this).val(keyValue);
+												});
+												
+							
+						},
+						
+						change: function() {
 								// First, dehighlight previously highlighted entries
 								var selected = $( "table input.display_bright");
 								selected.toggleClass("display_bright",false);
-								$(this).toggleClass("display_bright");
-								//$(this).css("background-color","#FFFF66");
-								//$(this).fadeout(2000);
+								//selected.toggleClass("highlight_text",false);
+								var id = $(this).attr("id");
+								var selector = "[id$=i"+idVal(id)+"]";
+								var $currentInputs = $("input"+selector);
+								$currentInputs.toggleClass("display_bright",true);
+								//$currentInputs.toggleClass("highlight_text",true);
 								
 								console.log("change event");
 							},
-							select: function() {
+							
+						select: function() {
 								console.log("SELECT");
 								$(this).css("color","#660066");
 							},
+					}, "input" );	
+	
+
+	$("table#quote").on({ 	
+							
+							
 							keyup : function(event) {
 								
 								
@@ -126,33 +175,74 @@ $(document).ready(function()
 								var cursorPosition = $(this).getCursorPosition();
 								// treat character keys as arrows
 								if( whichKey >36 && (whichKey < 41) ){
+												
 									switch (whichKey)
 									{
 										case 37: 
-											if (cursorPosition <= lastCursorPosition()) {
+											//if (cursorPosition <= lastCursorPosition()) {
 												console.log("Going to previous input");
+												/*
 												currentKey = $(this).closest("td").attr("id");
 												prevKey=  "#" + currentKey.substring(0,3) + (Number(currentKey.substring(3))-1);
 												$(prevKey + " input").select();
-										
+												*/
+												currentID = $(this).attr("id");
+												previous = "#" + idTag(currentID) + Math.max( idVal(currentID)-1,1);
+												console.log("PreviousID="+previous);
+												//previousIndex = Math.max( 0, currentIndex-1);
+												//var previous = $("td").eq(previousIndex);
+												$(previous).select();
 												//console.log("SELECT: keywhich[" + whichKey +"]");
-											};
+											//};
 											break;
 										case 39:
-											if (cursorPosition == 	lastCursorPosition()) {
+											//if (cursorPosition == 	lastCursorPosition()) {
 												console.log("Going to next input");
 												console.log("SELECT: keywhich[" + whichKey +"]");
-												currentKey = $(this).closest("td").attr("id");
-												nextKey=  "#" + currentKey.substring(0,3) + (Number(currentKey.substring(3))+1);
-												$(nextKey + " input").select();
+												//currentKey = $(this).closest("td").attr("id");
+												//nextKey=  "#" + currentKey.substring(0,3) + (Number(currentKey.substring(3))+1);
+												//$(nextKey + " input").select();
+												currentID = $(this).attr("id");
+												next = "#" + idTag(currentID) + Math.min( idVal(currentID)+1,Solution().length-1);
+												$(next).select();
 										
-											};
+											//};
 											break;
 										case 38 :
-											$("#quote input:first").focus();
+											//$("#quote input:first").focus();
+											var $tr = $(this).closest("tr").prev();
+											if ($tr.exists())
+											{
+												var currentIndex = $(this).closest("td").index();
+												console.log("index ="+currentIndex);
+												var aboveID = $($tr).children().eq(currentIndex).attr("id");
+												
+												if (aboveID)
+												{	
+													console.log("previousID=" + aboveID);
+													newID = "#"+aboveID;
+													$(newID + " input").select();
+												}
+												//$(previous + " input").select();
+											}
 											break;
 										case 40:
-											$("#quote input:last").focus();
+											//$("#quote input:last").focus();
+											var $tr = $(this).closest("tr").next();
+											if ($tr.exists())
+											{
+												var currentIndex = $(this).closest("td").index();
+												console.log("index ="+currentIndex);
+												var belowID = $($tr).children().eq(currentIndex).attr("id");
+												
+												if (belowID)
+												{	
+													console.log("nextID=" + belowID);
+													newID = "#" + belowID;
+													$(newID + " input").select();
+												}
+												//$(previous + " input").select();
+											}
 											break;
 									} 							
 								}
@@ -168,14 +258,15 @@ $(document).ready(function()
 											//lastCursorPosition( -1);
 											//$(this).get(0).focus();
 											console.log("Go to the next input field");
-											currentKey = $(this).closest("td").attr("id");
-											nextKey=  "#" + currentKey.substring(0,3) + (Number(currentKey.substring(3))+1);
+											currentID = $(this).closest("td").attr("id");
+											nextKey=  "#" + idTag(currentID) + Math.min(idVal(currentID)+1,Solution().length-1)	;
 											
 											$(nextKey + " input").select();
 											
 											
 										} 
 									}
+									else $(this).select();
 									
 								}
 								/*
@@ -222,7 +313,6 @@ $(document).ready(function()
 							}						
 						},
 						"input");
-	
 });
 			
 		
